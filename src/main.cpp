27f295af542a740dde59cd4d328aba3da41cd68a
Soham Roy <sohamroy@Sohams-MacBook-Air.local>
2026-06-18 11:29:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,13 @@ int main() {
     {
       std::cout<<command.substr(5)<<std::endl;
     }
-    else if(command.substr(0,5) == "type " && (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type"))
-      std::cout<<command.substr(5)<<" is a shell builtin"<<std::endl;
+    else if(command.substr(0,5) == "type " )
+    {
+      if((command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type"))
+        std::cout<<command.substr(5)<<" is a shell builtin"<<std::endl;
+      else
+        std::cout<<command.substr(5)<<": not found"<<std::endl;
+    }
     else
       std::cout<<command<<": not found"<<std::endl;
   }
